327d39d99fea896fd86cb7e53bce97ce9a5a16e3
laravdorna <lvazquezdorna@danielcastelao.org>
2020-02-20 13:06:23 +0100

    actualiza, creada nuevo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
 import {FormsModule} from '@angular/forms';
+import { ProductoBComponent } from './producto-b/producto-b.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent
+    ProductosComponent,
+    ProductoBComponent
   ],
   imports: [
     BrowserModule,
